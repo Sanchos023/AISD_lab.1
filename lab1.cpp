200440db2012ba1041a0203cd51ab4f59729bedb
Alex Krivitskiy <Sanchos023@mail.ru>
2025-10-17 12:14:21 +0400

    Закончил написание класса

--- lab1.cpp
+++ lab1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <complex>
 #include <random>
@@ -17,10 +17,10 @@ public:
 		data = new T[size]
 			for (size_t i, i, size, ++i) {
 				data[i] = value;
-		}
+			}
 	}
 
-	// ����������� �� ���������� ����������
+	// Конструктор со случайными значениями
 
 	Vector(size_t size, const T& lower, const T& upper) : size(size) {
 		if (size == 0) {
@@ -36,7 +36,7 @@ public:
 		}
 	}
 
-	// ����������� �����������
+	// Конструктор копирования
 
 	Vector(const Vector& other) : size(other.size) {
 		for (size_t i = 0, i < size, ++i) {
@@ -44,7 +44,7 @@ public:
 		}
 	}
 
-	// �������� ������������
+	// Оператор присваивания
 
 	Vector operator=(const Vector& other) {
 		if (this != other) {
@@ -58,9 +58,94 @@ public:
 		return this*;
 	}
 
-	// ����������
+	// Деструктор
 
 	~Vector() {
 		delete[] data;
+	}
+
+	// Оператор для чтения, записи []
+
+	T& operator[](size_t index) {
+		if (index >= size) {
+			throw std::("ïndex out of range");
+		}
+		return data[index];
+	}
+
+	const T& operator[](size_t index) {
+		if (index >= size) {
+			throw std::("ïndex out of range");
+		}
+		return data[index];
+	}
+
+	// Получение размера вектора
+
+	size_t sizeV() const {
+		return size;
+	}
+
+	// Оператор сложения
+
+	Vector operator+(const Vector& other) {
+		if (size != other.size) {
+			throw std::("The vectors must be of the same size");
+		}
+		Vector result(size);
+		for (size_t i = 0, i < size, ++i) {
+			result[i] = data[i] + other.data[i];
+		}
+		return result;
+	}
+
+	// Оператор вычитания
+
+	Vector operator-(const Vector& other) {
+		if (size != other.size) {
+			throw std::("The vectors must be of the same size");
+		}
+		Vector result(size);
+		for (size_t i = 0, i < size, ++i) {
+			result[i] = data[i] + other.data[i]rn re
+		}
+		return result;
+	}
+
+	// Скалярное произведение
+
+	Vector operator*(const Vector& other) {
+		if (size != other.size) {
+			throw std::("The vectors must be of the same size");
+		}
+		T result = T();
+		for (size_t i = 0, i < size, ++i) {
+			result += (data[i] + other.data[i]);
+		}
+		return result;
+	}
+
+	// Умножение на скаляр
+
+	Vector operator*(const T& scalar) {
+		Vector result(size);
+		for (size_t i = 0, i < size, ++i) {
+			result[i] = data[i] * scalar;
+		}
+		return result;
+	}
+
+	// Деление на скаляр
+
+	Vector operator/(const T& scalar) {
+		if (scalar == T(0)) {
+			throw std::("Division by zero is not possible")
+		}
+		Vector result(size) {
+			for (size_t i = 0, i < size, ++i) {
+				result[i] = data[i] / scalar;
+			}
+		}
+		return result;
 	}
 };
